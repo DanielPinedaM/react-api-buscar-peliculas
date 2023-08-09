4b2c8cb2f9d4198fb32739508b5980dd057f9914
daniel <daniel.pinedamo@amigo.edu.co>
2023-08-08 21:47:50 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,157 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import './App.css';
+import { Movies } from './components/Movies.jsx';
+import { useMovies } from './hooks/useMovies.jsx';
+/* import debounce from 'just-debounce-it' */
+
+function useSearch () {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+
+    if (search === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return { search, updateSearch, error }
+}
+
+function App () {
+  const [sort, setSort] = useState(false)
+
+  const { search, updateSearch, error } = useSearch()
+  const { movies, loading, getMovies } = useMovies({ search, sort })
+
+  function debounce(fn, wait, callFirst) {
+  var timeout = null;
+  var debouncedFn = null;
+
+  var clear = function() {
+    if (timeout) {
+      clearTimeout(timeout);
+
+      debouncedFn = null;
+      timeout = null;
+    }
+  };
+
+  var flush = function() {
+    var call = debouncedFn;
+    clear();
+
+    if (call) {
+      call();
+    }
+  };
+
+  var debounceWrapper = function() {
+    if (!wait) {
+      return fn.apply(this, arguments);
+    }
+
+    var context = this;
+    var args = arguments;
+    var callNow = callFirst && !timeout;
+    clear();
+
+    debouncedFn = function() {
+      fn.apply(context, args);
+    };
+
+    timeout = setTimeout(function() {
+      timeout = null;
+
+      if (!callNow) {
+        var call = debouncedFn;
+        debouncedFn = null;
+
+        return call();
+      }
+    }, wait);
+
+    if (callNow) {
+      return debouncedFn();
+    }
+  };
+
+  debounceWrapper.cancel = clear;
+  debounceWrapper.flush = flush;
+
+  return debounceWrapper;
+  }
+
+  const debouncedGetMovies = useCallback(
+    debounce(search => {
+      console.log('search', search)
+      getMovies({ search })
+    }, 300)
+    , [getMovies]
+  )
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    getMovies({ search })
+  }
+
+  const handleSort = () => {
+    setSort(!sort)
+  }
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value
+    updateSearch(newSearch)
+    debouncedGetMovies(newSearch)
+  }
+
+  return (
+    <div className='page'>
+
+      <header>
+        <h1>Buscador de películas</h1>
+        <form className='form' onSubmit={handleSubmit}>
+          <input
+            style={{
+              border: '1px solid transparent',
+              borderColor: error ? 'red' : 'transparent'
+            }} onChange={handleChange} value={search} name='query' placeholder='Avengers, Star Wars, The Matrix...'
+          />
+          <button type='submit'>Buscar</button>
+          <div>
+            <input type='checkbox' onChange={handleSort} checked={sort} />
+            <span>ordenar alfabeticamente</span>
+          </div>
+         
+        </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </header>
+
+      <main>
+        {
+          loading ? <p>Cargando...</p> : <Movies movies={movies} />
+        }
+      </main>
+    </div>
+  )
+}
+
+export default App
